04bd9307bf96a8aaf066c7156be3599bcea056c7
jgarcia18201 <jgarcia18201@gmail.com>
2025-03-05 17:44:22 -0600

    included version information on load

--- CSharp/HTMLParser/Form1.cs
+++ CSharp/HTMLParser/Form1.cs
@@ -22,9 +22,20 @@ namespace HTMLParser
             
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) 
         {
-            this.txtResult.Text = "Data";
+            var notify = new System.Text.StringBuilder();
+            var assembly_version = System.Reflection.Assembly.GetEntryAssembly().GetName().Version;
+            var versionNumber = assembly_version.Major;
+            //foreach (var property_def in assembly_version.GetType().GetProperties())
+            //{
+            //    if (string.Equals(property_def.Name, "major", StringComparison.CurrentCultureIgnoreCase))
+            //    {
+            //        //notify.AppendLine($"{property_def.Name}: {property_def.GetValue(assembly_version, null).ToString()}");
+            //        versionNumber = property_def.GetValue(assembly_version, null).ToString();
+            //    }
+            //}
+            this.txtResult.Text = $"{nameof(versionNumber)}: {versionNumber}";
         }
     }
 }
